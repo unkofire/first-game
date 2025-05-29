50405bc8503a8cc7c7f2043543dd62e737414c97
unkofire <146385371+unkofire@users.noreply.github.com>
2025-05-28 21:09:03 -0700

    Audio, Pause Menu
    Added more functionality to the pause menu, started working on audio.

--- Assets/Scripts/PauseMenu.cs
+++ Assets/Scripts/PauseMenu.cs
@@ -1,17 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PauseMenu : MonoBehaviour
 {
-    public GameObject pausemenu;
+    public GameObject pauseMenuObj;
     public GameObject quitMenu;
     public GameObject mainMenu;
+    public GameObject yes_no;
+    public Animator animatorController; 
     private bool isPaused = false;
     // Start is called before the first frame update
     void Start()
     {
-        pausemenu.SetActive(false);
+        pauseMenuObj.SetActive(false);
         quitMenu.SetActive(false);
         mainMenu.SetActive(false); 
     }
@@ -35,7 +38,7 @@ public class PauseMenu : MonoBehaviour
 
     public void pauseGame()
     {
-        pausemenu.SetActive(true);
+        pauseMenuObj.SetActive(true);
         Time.timeScale = 0; // takes this number, times it by time.  
         Cursor.visible = true; // cursor not visible 
         Cursor.lockState = CursorLockMode.None; // keeps cursors in the middle of the screen
@@ -45,19 +48,71 @@ public class PauseMenu : MonoBehaviour
 
     public void unpauseGame()
     {
-        pausemenu.SetActive(false);
+        
+        closeQuitConfirm();
+        closeMainMenuConfirm();
         Time.timeScale = 1; // takes this number, times it by time. 
         Cursor.visible = false; // cursor not visible 
         Cursor.lockState = CursorLockMode.Locked; // keeps cursors in the middle of the screen
+        pauseMenuObj.SetActive(false);
         isPaused = false;
     }
 
-    public void OpenQuitConfirm()
+    public void openQuitConfirm()
     {
         quitMenu.SetActive(true);
-        pausemenu.SetActive(false);
+        pauseMenuObj.SetActive(false);
 
     }
 
-    
+    public void closeQuitConfirm()
+    {
+        quitMenu.SetActive(false);
+        pauseMenuObj.SetActive(true);
+        yes_no.SetActive(false);
+    }
+
+    public void quitGame()
+    {
+        Application.Quit();
+    }
+
+    public void openMainMenuConfirm()
+    {
+        mainMenu.SetActive(true);
+        pauseMenuObj.SetActive(false);
+    }
+
+    public void closeMainMenuConfirm()
+    {
+        mainMenu.SetActive(false);
+        pauseMenuObj.SetActive(true);
+        yes_no.SetActive(false);
+    }
+
+    public void goToMainMenu()
+    {
+        SceneManager.LoadScene(0);
+        Time.timeScale = 1; // takes this number, times it by time. 
+    }
+
+    public void hoverYes()
+    {
+       
+        yes_no.SetActive(true);
+        animatorController.Play("yes animation"); 
+    }
+
+    public void hoverNo()
+    {
+        yes_no.SetActive(true);
+        animatorController.Play("no animation");
+    }
+
+    public void hoverExit()
+    {
+        
+        yes_no.SetActive(false);
+    }
+
 }
